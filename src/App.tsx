e5c66a04dba3c41d2ac0b493b359f015b00b0cdb
Pavel Gordienko <guzmo@yandex.ru>
2024-11-07 09:21:28 +0300

    feat: add main structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,105 @@
-
 import './App.css'
 
 function App() {
-
   return (
-    <h1 className="text-2xl font-bold">
-      Hello
-    </h1>
-  )
+    <main className="flex justify-center items-center h-screen bg-gradient-to-br from-yellow-700 via-yellow-400 to-yellow-200">
+      <div className="flex justify-center items-center space-x-0">
+
+        {/* block */}
+        <div className=" py-5 w-[100px] h-[400px] bg-[url('/images/Frame1.png')] bg-cover bg-center flex flex-col justify-between items-center">
+          {/* up */}
+          <div className='flex flex-col'>
+            <div className="w-[66px] h-[31px] bg-[url('/images/Vector1.png')]"></div>
+          </div>
+
+          {/* down */}
+          <div className='flex flex-col space-y-1'>
+            <div className="w-[66px] h-[31px] bg-[url('/images/Vector1.png')]"></div>
+            <div className="w-[66px] h-[31px] bg-[url('/images/Vector1.png')]"></div>
+            <div className="w-[66px] h-[31px] bg-[url('/images/Vector1.png')]"></div>
+            <div className="w-[66px] h-[31px] bg-[url('/images/Vector1.png')]"></div>
+            <div className="w-[66px] h-[31px] bg-[url('/images/Vector1.png')]"></div>
+            <div className="w-[66px] h-[31px] bg-[url('/images/Vector1.png')]"></div>
+            <div className="w-[66px] h-[31px] bg-[url('/images/Vector1.png')]"></div>
+
+          </div>
+        </div>
+
+        {/* block */}
+        <div className=" py-5 w-[100px] h-[400px] bg-[url('/images/Frame1.png')] bg-cover bg-center flex flex-col justify-between items-center">
+          {/* up */}
+          <div className='flex flex-col'>
+            <div className="w-[66px] h-[31px] bg-[url('/images/Vector1.png')]"></div>
+          </div>
+
+          {/* down */}
+          <div className='flex flex-col space-y-1'>
+            <div className="w-[66px] h-[31px] bg-[url('/images/Vector1.png')]"></div>
+            <div className="w-[66px] h-[31px] bg-[url('/images/Vector1.png')]"></div>
+            <div className="w-[66px] h-[31px] bg-[url('/images/Vector1.png')]"></div>
+            <div className="w-[66px] h-[31px] bg-[url('/images/Vector1.png')]"></div>
+            <div className="w-[66px] h-[31px] bg-[url('/images/Vector1.png')]"></div>
+
+          </div>
+        </div>
+
+ 
+        {/* block */}
+        <div className=" py-5 w-[100px] h-[400px] bg-[url('/images/Frame1.png')] bg-cover bg-center flex flex-col justify-between items-center">
+          {/* up */}
+          <div className='flex flex-col'>
+            <div className="w-[66px] h-[31px] bg-[url('/images/Vector1.png')]"></div>
+          </div>
+
+          {/* down */}
+          <div className='flex flex-col space-y-1'>
+            <div className="w-[66px] h-[31px] bg-[url('/images/Vector1.png')]"></div>
+            <div className="w-[66px] h-[31px] bg-[url('/images/Vector1.png')]"></div>
+            <div className="w-[66px] h-[31px] bg-[url('/images/Vector1.png')]"></div>
+
+          </div>
+        </div>
+
+        {/* block */}
+        <div className=" py-5 w-[100px] h-[400px] bg-[url('/images/Frame1.png')] bg-cover bg-center flex flex-col justify-between items-center">
+          {/* up */}
+          <div className='flex flex-col'>
+            <div className="w-[66px] h-[31px] bg-[url('/images/Vector1.png')]"></div>
+          </div>
+
+          {/* down */}
+          <div className='flex flex-col space-y-1'>
+            <div className="w-[66px] h-[31px] bg-[url('/images/Vector1.png')]"></div>
+            <div className="w-[66px] h-[31px] bg-[url('/images/Vector1.png')]"></div>
+            <div className="w-[66px] h-[31px] bg-[url('/images/Vector1.png')]"></div>
+            <div className="w-[66px] h-[31px] bg-[url('/images/Vector1.png')]"></div>
+            <div className="w-[66px] h-[31px] bg-[url('/images/Vector1.png')]"></div>
+
+          </div>
+        </div>
+
+        {/* block */}
+        <div className=" py-5 w-[100px] h-[400px] bg-[url('/images/Frame1.png')] bg-cover bg-center flex flex-col justify-between items-center">
+          {/* up */}
+          <div className='flex flex-col'>
+            <div className="w-[66px] h-[31px] bg-[url('/images/Vector1.png')]"></div>
+          </div>
+
+          {/* down */}
+          <div className='flex flex-col space-y-1'>
+            <div className="w-[66px] h-[31px] bg-[url('/images/Vector1.png')]"></div>
+            <div className="w-[66px] h-[31px] bg-[url('/images/Vector1.png')]"></div>
+            <div className="w-[66px] h-[31px] bg-[url('/images/Vector1.png')]"></div>
+            <div className="w-[66px] h-[31px] bg-[url('/images/Vector1.png')]"></div>
+            <div className="w-[66px] h-[31px] bg-[url('/images/Vector1.png')]"></div>
+
+          </div>
+        </div>
+
+
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
